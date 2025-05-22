fed46b60da672797afd46cc7c44a9bcadcf93309
Davide Corio <enlightx@gmail.com>
2025-05-22 17:18:39 +0200

    [ADD] carcolor_stock

---
+++ carcolor_stock/__manifest__.py
@@ -0,0 +1,18 @@
+
+{
+    "name": "Carcolor - Stock",
+    "summary": "Personalizzazioni su magazzino",
+    "version": "18.0.1.0.0",
+    "category": "Carcolor",
+    "website": "",
+    "author": "Carcolor",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_it_stock_ddt",
+        "db_reporting_carbone",
+    ],
+    "data": [
+        "reports/ddt.xml",
+        "views/stock_views.xml",
+    ]
+}
