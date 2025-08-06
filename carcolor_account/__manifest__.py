17eb28810edeb1550688f8fefa73eec5d305120d
Davide Corio <enlightx@gmail.com>
2025-08-06 09:07:35 +0200

    [ADD][carcolor_account] invoice template

--- carcolor_account/__manifest__.py
+++ carcolor_account/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Carcolor - Account",
     "summary": "Personalizzazioni su contabilità e amministrazione",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Carcolor",
     "website": "",
     "author": "Carcolor",
